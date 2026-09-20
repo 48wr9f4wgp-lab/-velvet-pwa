8fc8ea145c02e9a7605d491d65c981f77b2931a5
48wr9f4wgp-lab <48wr9f4wgp@privaterelay.appleid.com>
2026-09-20 17:59:10 +0900

    Velvet v49.4: load fullscreen swipe UX

--- src/input-guard.js
+++ src/input-guard.js
@@ -1,9 +1,9 @@
 import "./ui-ja.js?v=48";
 import "./flow-feedback.js?v=44";
 import "./feed-bridge.js?v=44";
-import "./media-viewer.js?v=49.3";
+import "./media-viewer.js?v=49.4";
 
-const UX_STYLESHEET = "./flow-ux.css?v=49.3";
+const UX_STYLESHEET = "./flow-ux.css?v=49.4";
 if (!document.querySelector('link[data-velvet-flow-ux]')) {
   const link = document.createElement("link");
   link.rel = "stylesheet";
